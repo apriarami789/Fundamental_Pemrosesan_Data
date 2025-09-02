49713faaba3c21dca30c6d4a5233d9c798cbe628
Mhd. Apri Arami <150607943+apriarami789@users.noreply.github.com>
2025-09-02 17:33:08 +0700

    update file transform.py

--- utils/transform.py
+++ utils/transform.py
@@ -71,10 +71,10 @@ def change_data_type(data):
 
     # 4. Tangani kolom timestamp secara terpisah karena logikanya kondisional
     if 'scraped_at' in data.columns:
-        data['timestamp'] = pd.to_datetime(data['scraped_at'])
+        data['scraped_at'] = pd.to_datetime(data['scraped_at'])
     else:
         # Fallback jika tidak ada kolom 'scraped_at'
-        data['timestamp'] = pd.Timestamp.now()
+        data['scraped_at'] = pd.Timestamp.now()
 
     print("Tipe data setiap kolom sudah diubah dan juga sudah di transformasi.")
     return data
@@ -82,7 +82,7 @@ def change_data_type(data):
 # Pilih dan urutkan kolom akhir
 def fix_column_data(data):
     try:
-        df_transform = data[['product_name', 'price', 'rating', 'color', 'size', 'gender', 'timestamp']]
+        df_transform = data[['product_name', 'price', 'rating', 'color', 'size', 'gender', 'scraped_at']]
         return df_transform
     except KeyError as e:
         print(f"Ada Kolom yang tidak ditemukan: {e}")
